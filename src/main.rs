670440c730cbf751355e35f622a9bf326aac8af6
Wesley Moore <wes@wezm.net>
2021-09-24 16:24:57 +1000

    Initial version that compiles

---
+++ src/main.rs
@@ -0,0 +1,249 @@
+#![no_main]
+#![no_std]
+
+use panic_halt as _;
+
+use embedded_hal::digital::v2::OutputPin;
+use rtic::app;
+use stm32f3xx_hal::gpio::{Input, Output, PushPull, PXx};
+use stm32f3xx_hal::prelude::*;
+use stm32f3xx_hal::usb::{Peripheral, UsbBus, UsbBusType};
+use stm32f3xx_hal::{pac, timer};
+use usb_device::bus::UsbBusAllocator;
+use usb_device::class::UsbClass as _;
+
+use keyberon::action::Action::{self, *};
+use keyberon::action::{d, k, l, m, HoldTapConfig};
+use keyberon::debounce::Debouncer;
+use keyberon::key_code::KeyCode::*;
+use keyberon::key_code::{KbHidReport, KeyCode};
+use keyberon::layout::Layout;
+use keyberon::matrix::{Matrix, PressedKeys};
+
+type UsbClass = keyberon::Class<'static, UsbBusType, ()>;
+type UsbDevice = usb_device::device::UsbDevice<'static, UsbBusType>;
+
+// pub struct Leds {
+//     caps_lock: gpio::gpioc::PC13<gpio::Output<gpio::PushPull>>,
+// }
+// impl keyberon::keyboard::Leds for Leds {
+//     fn caps_lock(&mut self, status: bool) {
+//         if status {
+//             self.caps_lock.set_low().unwrap()
+//         } else {
+//             self.caps_lock.set_high().unwrap()
+//         }
+//     }
+// }
+
+const CUT: Action = m(&[LShift, Delete]);
+const COPY: Action = m(&[LCtrl, Insert]);
+const PASTE: Action = m(&[LShift, Insert]);
+const C_ENTER: Action = HoldTap {
+    timeout: 200,
+    config: HoldTapConfig::HoldOnOtherKeyPress,
+    tap_hold_interval: 0,
+    hold: &k(LCtrl),
+    tap: &k(Enter),
+};
+const L1_SP: Action = HoldTap {
+    timeout: 200,
+    config: HoldTapConfig::Default,
+    tap_hold_interval: 0,
+    hold: &l(1),
+    tap: &k(Space),
+};
+const CENTER: Action = m(&[LCtrl, Enter]);
+
+#[rustfmt::skip]
+pub static LAYERS: keyberon::layout::Layers = &[
+    &[
+        &[k(Grave),   k(Kb1),k(Kb2),k(Kb3), k(Kb4),k(Kb5),k(KpMinus),k(KpSlash),k(KpAsterisk),k(Kb6),   k(Kb7), k(Kb8), k(Kb9),  k(Kb0),   k(Minus)   ],
+        &[k(Tab),     k(Q),  k(W),  k(E),   k(R),  k(T),     k(Kp7), k(Kp8),    k(Kp9),       k(Y),     k(U),   k(I),   k(O),    k(P),     k(LBracket)],
+        &[k(RBracket),k(A),  k(S),  k(D),   k(F),  k(G),     k(Kp4), k(Kp5),    k(Kp6),       k(H),     k(J),   k(K),   k(L),    k(SColon),k(Quote)   ],
+        &[k(Equal),   k(Z),  k(X),  k(C),   k(V),  k(B),     k(Kp1), k(Kp2),    k(Kp3),       k(N),     k(M),   k(Comma),k(Dot), k(Slash), k(Bslash)  ],
+        &[Trans,      Trans, k(LGui),k(LAlt),L1_SP,k(LShift),k(Kp0), k(KpDot),  k(KpPlus),    k(RShift),C_ENTER,k(RAlt),k(BSpace),Trans,   Trans      ],
+    ], &[
+        &[k(F1),k(F2),k(F3),     k(F4),k(F5),    k(F6),Trans,Trans,Trans,k(F7),      k(F8),  k(F9),    k(F10), k(F11),  k(F12)],
+        &[Trans,Trans,Trans,     Trans,Trans,    Trans,Trans,Trans,Trans,Trans,      Trans,  k(Delete),Trans,  Trans,   Trans ],
+        &[d(0), d(1), k(NumLock),Trans,k(Escape),Trans,Trans,Trans,Trans,k(CapsLock),k(Left),k(Down),  k(Up),  k(Right),Trans ],
+        &[Trans,Trans,CUT,       COPY, PASTE,    Trans,Trans,Trans,Trans,Trans,      k(Home),k(PgDown),k(PgUp),k(End),  Trans ],
+        &[Trans,Trans,Trans,     Trans,Trans,    Trans,Trans,Trans,Trans,Trans,      CENTER, Trans,    Trans,  Trans,   Trans ],
+    ],
+];
+
+#[app(device = stm32f3xx_hal::pac, peripherals = true)]
+const APP: () = {
+    struct Resources {
+        usb_dev: UsbDevice,
+        usb_class: UsbClass,
+        matrix: Matrix<PXx<Output<PushPull>>, PXx<Input>, 8, 10>,
+        debouncer: Debouncer<PressedKeys<8, 10>>,
+        layout: Layout,
+        timer: timer::Timer<pac::TIM3>,
+    }
+
+    #[init]
+    fn init(c: init::Context) -> init::LateResources {
+        static mut USB_BUS: Option<UsbBusAllocator<UsbBusType>> = None;
+
+        let mut flash = c.device.FLASH.constrain();
+        let mut rcc = c.device.RCC.constrain();
+
+        // set 0x424C in DR10 for dfu on reset
+        // let bkp = rcc
+        //     .bkp
+        //     .constrain(c.device.BKP, &mut rcc.apb1, &mut c.device.PWR);
+        // bkp.write_data_register_low(9, 0x424C);
+
+        let clocks = rcc
+            .cfgr
+            .use_hse(8.MHz())
+            .sysclk(48.MHz())
+            .pclk1(24.MHz())
+            .pclk2(24.MHz())
+            .freeze(&mut flash.acr);
+
+        assert!(clocks.usbclk_valid());
+
+        let mut gpioa = c.device.GPIOA.split(&mut rcc.ahb);
+        let mut gpiob = c.device.GPIOB.split(&mut rcc.ahb);
+        let mut gpioc = c.device.GPIOC.split(&mut rcc.ahb);
+
+        // USB is on PA11 (D-) and PA12 (D+)
+        // Pull the D+ pin down to send a RESET condition to the USB bus.
+        let mut usb_dp = gpioa.pa12.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+        usb_dp.set_low().unwrap();
+        cortex_m::asm::delay(clocks.sysclk().0 / 100);
+
+        // let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
+        // led.set_high().unwrap();
+        let leds = (); // Leds { caps_lock: led };
+
+        let usb_dm = gpioa.pa11.into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+        let usb_dp = usb_dp.into_af14_push_pull(&mut gpioa.moder, &mut gpioa.otyper, &mut gpioa.afrh);
+
+        let usb = Peripheral {
+            usb: c.device.USB,
+            pin_dm: usb_dm,
+            pin_dp: usb_dp,
+        };
+
+        *USB_BUS = Some(UsbBus::new(usb));
+        let usb_bus = USB_BUS.as_ref().unwrap();
+
+        let usb_class = keyberon::new_class(usb_bus, leds);
+        let usb_dev = keyberon::new_device(usb_bus);
+
+        // Set up the matrix scan timer, polls at 1kHz (1000 times a second/every 1ms)
+        let mut timer =
+            timer::Timer::new(c.device.TIM3, clocks, &mut rcc.apb1);
+        timer.start(1.milliseconds());
+        timer.enable_interrupt(timer::Event::Update);
+
+        /*
+            cols
+            palSetPadMode(GPIOB, 10, PAL_MODE_OUTPUT_PUSHPULL);
+            palSetPadMode(GPIOB, 2, PAL_MODE_OUTPUT_PUSHPULL);
+            palSetPadMode(GPIOB, 1,  PAL_MODE_OUTPUT_PUSHPULL);
+            palSetPadMode(GPIOB, 0,  PAL_MODE_OUTPUT_PUSHPULL);
+            palSetPadMode(GPIOA, 7,  PAL_MODE_OUTPUT_PUSHPULL);
+            palSetPadMode(GPIOB, 4,  PAL_MODE_OUTPUT_PUSHPULL);
+            palSetPadMode(GPIOB, 3,  PAL_MODE_OUTPUT_PUSHPULL);
+            palSetPadMode(GPIOB, 7,  PAL_MODE_OUTPUT_PUSHPULL);
+
+            rows
+            palSetPadMode(GPIOB, 11, PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOA, 6,  PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOA, 3,  PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOA, 2, PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOA, 1, PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOB, 5, PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOB, 6, PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOC, 15,  PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOC, 14,  PAL_MODE_INPUT_PULLDOWN);
+            palSetPadMode(GPIOC, 13,  PAL_MODE_INPUT_PULLDOWN);
+
+            #define MATRIX_ROW_PINS { B11, A6, A3, A2, A1, B5, B6, C15, C14, C13 }
+            #define MATRIX_COL_PINS { B10, B2, B1, B0, A7, B4, B3, B7 }
+            #define UNUSED_PINS { A0, A8, A15, B12, B13, B14, B15 }
+            #define DIODE_DIRECTION COL2ROW
+
+            Board: 15 x 5
+            Defined above: 8 cols, 10 rows = 80 keys
+         */
+
+        let matrix = Matrix::new(
+            [
+                gpiob.pb10.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper).downgrade().downgrade(),
+                gpiob.pb2.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper).downgrade().downgrade(),
+                gpiob.pb1.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper).downgrade().downgrade(),
+                gpiob.pb0.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper).downgrade().downgrade(),
+                gpioa.pa7.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper).downgrade().downgrade(),
+                gpiob.pb4.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper).downgrade().downgrade(),
+                gpiob.pb3.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper).downgrade().downgrade(),
+                gpiob.pb7.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper).downgrade().downgrade(),
+            ],
+            [
+                gpiob.pb11.into_pull_down_input(&mut gpiob.moder, &mut gpiob.pupdr).downgrade().downgrade(),
+                gpioa.pa6.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr).downgrade().downgrade(),
+                gpioa.pa3.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr).downgrade().downgrade(),
+                gpioa.pa2.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr).downgrade().downgrade(),
+                gpioa.pa1.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr).downgrade().downgrade(),
+                gpiob.pb5.into_pull_down_input(&mut gpiob.moder, &mut gpiob.pupdr).downgrade().downgrade(),
+                gpiob.pb6.into_pull_down_input(&mut gpiob.moder, &mut gpiob.pupdr).downgrade().downgrade(),
+                gpioc.pc15.into_pull_down_input(&mut gpioc.moder, &mut gpioc.pupdr).downgrade().downgrade(),
+                gpioc.pc14.into_pull_down_input(&mut gpioc.moder, &mut gpioc.pupdr).downgrade().downgrade(),
+                gpioc.pc13.into_pull_down_input(&mut gpioc.moder, &mut gpioc.pupdr).downgrade().downgrade(),
+            ],
+        );
+
+        init::LateResources {
+            usb_dev,
+            usb_class,
+            timer,
+            debouncer: Debouncer::new(PressedKeys::default(), PressedKeys::default(), 5),
+            matrix: matrix.unwrap(),
+            layout: Layout::new(LAYERS),
+        }
+    }
+
+    #[task(binds = USB_HP_CAN_TX, priority = 2, resources = [usb_dev, usb_class])]
+    fn usb_tx(mut c: usb_tx::Context) {
+        usb_poll(&mut c.resources.usb_dev, &mut c.resources.usb_class);
+    }
+
+    #[task(binds = USB_LP_CAN_RX0, priority = 2, resources = [usb_dev, usb_class])]
+    fn usb_rx(mut c: usb_rx::Context) {
+        usb_poll(&mut c.resources.usb_dev, &mut c.resources.usb_class);
+    }
+
+    #[task(binds = TIM3, priority = 1, resources = [usb_class, matrix, debouncer, layout, timer])]
+    fn tick(mut c: tick::Context) {
+        c.resources.timer.clear_event(timer::Event::Update);
+
+        for event in c
+            .resources
+            .debouncer
+            .events(c.resources.matrix.get().unwrap())
+        {
+            c.resources.layout.event(event);
+        }
+        c.resources.layout.tick();
+        send_report(c.resources.layout.keycodes(), &mut c.resources.usb_class);
+    }
+};
+
+fn send_report(iter: impl Iterator<Item = KeyCode>, usb_class: &mut resources::usb_class<'_>) {
+    use rtic::Mutex;
+    let report: KbHidReport = iter.collect();
+    if usb_class.lock(|k| k.device_mut().set_keyboard_report(report.clone())) {
+        while let Ok(0) = usb_class.lock(|k| k.write(report.as_bytes())) {}
+    }
+}
+
+fn usb_poll(usb_dev: &mut UsbDevice, keyboard: &mut UsbClass) {
+    if usb_dev.poll(&mut [keyboard]) {
+        keyboard.poll();
+    }
+}
